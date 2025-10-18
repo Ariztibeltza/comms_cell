cd6e597dade66975520584c9ee1f5d331213ad70
Ariztibeltza <gmendiazubiaur@hotmail.com>
2025-10-18 09:21:50 +0200

    env created

--- test/client.py
+++ test/client.py
@@ -1,10 +1,12 @@
 import socket
+import cryptography
 
 client = socket.socket(socket.AF_BLUETOOTH,
                            socket.SOCK_STREAM,
                            socket.BTPROTO_RFCOMM)
 channel=10
 client.connect(("9c:29:76:03:c5:b8",channel))
+
 
 try:
     while True:
