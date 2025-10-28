375aecd8884099fe852bb56f08255b96384e0511
Ariztibeltza <gmendiazubiaur@hotmail.com>
2025-10-28 20:48:42 +0100

    First functional client & server

---
+++ client.py
@@ -0,0 +1,62 @@
+import socket
+import pyaudio
+import keyboard
+
+# Audio constants
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+
+# Server contants
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5000
+CHUNK = 1024
+
+# Test constants
+BUTTON_PRESSED = True
+
+## FUNCTIONS ##################################################################
+
+## MAIN #######################################################################
+
+p1 = pyaudio.PyAudio()
+p2 = pyaudio.PyAudio()
+micro_stream = p1.open(format=FORMAT,
+                      channels=CHANNELS,
+                      rate=RATE,
+                      input=True,
+                      frames_per_buffer=CHUNK)
+speaker_stream = p2.open(format=FORMAT,
+                      channels=CHANNELS,
+                      rate=RATE,
+                      output=True,
+                      frames_per_buffer=CHUNK)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_IP,SERVER_PORT))
+print(f" ~ Connected to: {SERVER_IP}:{SERVER_PORT}")
+
+try:
+    while True:
+        if BUTTON_PRESSED:
+            #print("     ~ Recording")
+            audio_data = micro_stream.read(CHUNK,
+                                       exception_on_overflow=False)
+            client.sendall(audio_data)
+        else:
+            print("     ~ Reciving")
+            data = client.recv(CHUNK)
+            if not data:
+                break
+            speaker_stream.write(data)
+except OSError:                                      
+    None
+finally:
+    client.close()
+    micro_stream.stop_stream()
+    speaker_stream.stop_stream()
+    micro_stream.close()
+    speaker_stream.close()
+    p1.terminate()
+    p2.terminate()
--- server.py
+++ server.py
@@ -10,14 +10,30 @@ SERVER_IP = "127.0.0.1"
 #SERVER_IP = socket.gethostbyname(socket.gethostname())
 SERVER_PORT = 5000
 CHUNK = 1024
+CLIENT_LIST = []
 
 # FUNCTIONS ###################################################################
+
+def client_handler(conn,addr):
+    try:
+        while True:
+            client_data = conn.recv(CHUNK)
+            if client_data:
+                #print(f"    ~ Data from: {conn},{addr}")
+                for client in CLIENT_LIST:
+                    if conn!=client[0]:
+                        client[0].sendall(client_data)
+                        print(f"    ~ Data sent to {client[0]},{client[1]}")
+    except:
+        None
 
 def start():
     while True:
         conn,addr = server.accept()
-        # Create Thread
-
+        print(f"     ~ Connection accepted")
+        CLIENT_LIST.append([conn,addr])
+        thread = threading.Thread(target=client_handler,args=(conn,addr))
+        thread.start()
 
 
 # MAIN ########################################################################
@@ -26,4 +42,5 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((SERVER_IP,SERVER_PORT))
 print(f" ~ Server: {SERVER_IP}:{SERVER_PORT}")
 server.listen()
+start()
 
